e9f5df81d2229203528b280438ab3cfb69c407ef
sungmok <mostoo45@naver.com>
2018-06-30 11:29:04 +0900

    minor bug fixed

---
+++ model.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        
+        self.embed_size = embed_size
+        self.hidden_size= hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        
+    def forward(self, features, captions):
+        embeddings = self.embed(captions)
+        #embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        out, hidden =  self.lstm(embeddings)
+        outputs = self.linear(out)
+        return outputs
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        sampled_ids = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
+            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
+            _, predicted = outputs.max(1)                        # predicted: (batch_size)
+            #print(predicted.data.cpu().numpy()[0])
+            ab=predicted.data.cpu().numpy()[0]
+            sampled_ids.append(ab.astype(int))
+            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
+            inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+        #sampled_ids = torch.stack(sampled_ids, 1)
+        #print(sampled_ids.size)
+        return sampled_ids
